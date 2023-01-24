8c9f2965e21fccde4b12f2e35507081450f61eee
Brian Andersen <brianmanden@gmail.com>
2023-01-24 07:21:28 +0100

    ADD CustomerModel UPDATE view with new Customer model

---
+++ UploadFilesSln/UploadFilesApp/Data/CustomerModel.cs
@@ -0,0 +1,13 @@
+﻿using UploadFilesApp.Data;
+
+namespace UploadFilesApp.Data
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "someUserName";
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FileName { get; set; }
+    }
+}
