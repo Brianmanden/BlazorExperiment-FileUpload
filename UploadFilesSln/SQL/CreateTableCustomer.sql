abff50f972217b95ced3c633af06fb41e43d639e
Brian Andersen <brianmanden@gmail.com>
2023-01-28 10:10:58 +0100

    ADD SQL Create Customer Table ADD SQL Stored Proc Customer_Insert

---
+++ UploadFilesSln/SQL/CreateTableCustomer.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY Identity, 
+    [FirstName] NCHAR(50) NULL, 
+    [LastName] NCHAR(50) NULL, 
+    [FileName] NCHAR(2000) NULL, 
+    [UserName] NCHAR(50) NULL,
+)
---
+++ UploadFilesSln/SQL/spCustomer_Insert.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spCustomer_Insert]
+	@Id int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@FileName nvarchar(2000),
+	@UserName nvarchar(50)
+AS
+BEGIN
+	INSERT INTO dbo.Customer(FirstName, LastName, [FileName], UserName)
+	VALUES (@FirstName, @LastName, @FileName, @UserName);
+END
