74224868daf1daae43bced7febd093d4186604d6
Brian Andersen <brianmanden@gmail.com>
2023-01-29 20:58:22 +0100

    UPDATE SQL Data Access interface and class with load all customer functionality

--- UploadFilesSln/UploadFilesLibrary/DAL/SqlDataAccess.cs
+++ UploadFilesSln/UploadFilesLibrary/DAL/SqlDataAccess.cs
@@ -26,4 +26,18 @@ public class SqlDataAccess : ISqlDataAccess
             parameters,
             commandType: System.Data.CommandType.StoredProcedure);
     }
+
+    public async Task<List<T>> LoadData<T>(string storedProc, string connectionName, object? parameters) {
+        string connectionString = _config.GetConnectionString(connectionName)
+            ?? throw new Exception($"Missing connection string at {connectionName}");
+
+        using var connection = new SqlConnection(connectionString);
+
+        var rows = await connection.QueryAsync<T>(
+            storedProc,
+            parameters,
+            commandType: System.Data.CommandType.StoredProcedure);
+
+        return rows.ToList();
+    }
 }
--- UploadFilesSln/UploadFilesLibrary/Interfaces/ISqlDataAccess.cs
+++ UploadFilesSln/UploadFilesLibrary/Interfaces/ISqlDataAccess.cs
@@ -3,4 +3,5 @@
 public interface ISqlDataAccess
 {
     Task SaveData(string storedProc, string connectionName, object parameters);
+    Task<List<T>> LoadData<T>(string storedProc, string connectionName, object? parameters);
 }
